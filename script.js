ef67ec5209fd103f8a51ded2a6b22b6bbfff8cd5
instagramuser9091033708-cyber <instagramuser9091033708@gmail.com>
2026-03-28 09:49:28 +0530

    Refactor candle lighting and balloon creation logic

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let birthDate = new Date("2008-05-10");
 
+// loading
 setTimeout(()=>nextPage(2),3000);
 
 function nextPage(n){
@@ -12,6 +13,7 @@ function start(){
   nextPage(4);
 }
 
+// DOB
 window.onload = function(){
   let today = new Date();
   let age = today.getFullYear() - birthDate.getFullYear();
@@ -20,29 +22,31 @@ window.onload = function(){
   document.getElementById("age").innerText = "Age: " + age;
 };
 
+// candle
 function lightCandle(){
-  document.querySelector(".flame").style.opacity = 1;
+  document.querySelector(".flame").style.display="block";
 }
 
-let words=["You","are","a","cutie"];
-let i=0;
+// balloons
+let words = ["You","are","a","cutie"];
+let i = 0;
 
 function createBalloons(){
-  let box=document.getElementById("balloonBox");
-  let msg=document.getElementById("message");
+  let box = document.getElementById("balloonBox");
+  let msg = document.getElementById("message");
 
-  box.innerHTML="";
-  msg.innerText="";
-  i=0;
+  box.innerHTML = "";
+  msg.innerText = "";
+  i = 0;
 
   for(let j=0;j<4;j++){
-    let b=document.createElement("div");
-    b.className="balloon";
-    b.style.background=["red","blue","green","purple"][j];
+    let b = document.createElement("div");
+    b.className = "balloon";
+    b.style.background = ["red","blue","green","purple"][j];
 
-    b.onclick=function(){
-      if(i<words.length){
-        msg.innerText+=" "+words[i];
+    b.onclick = function(){
+      if(i < words.length){
+        msg.innerText += " " + words[i];
         i++;
       }
       b.remove();
